7137b474db2675df9417645bb1bd2bbdf0897585
fernandez-a <alejandro.fernandezarmas@gmail.com>
2022-01-19 19:01:58 +0100

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import {ApolloError, ApolloServer } from 'apollo-server'
 import {typeDefs} from "./schema"
-import {Query,Character,Episode} from "./resolvers/queries"
+import {Query,Character,Episode,Location} from "./resolvers/queries"
 const saltRounds = 10;
 const resolvers = {
     Query,
     Character,
-    Episode
+    Episode,
+    Location
 
   };
   const run = async () => {
@@ -15,7 +16,7 @@ const resolvers = {
   
     });
     server.listen(4000).then(() => {
-      console.log(`🚀  Server ready `);
+      console.log(`🚀  Server ready 4000`);
       });
   }
   try{
--- src/resolvers/queries.ts
+++ src/resolvers/queries.ts
@@ -4,10 +4,11 @@ import { EpisodeAPI, CharacterAPI, Last_Location, Location_json } from "../types
 export const Query = {
   character: async (parent: any, args: { id: number }) => {
     let character: CharacterAPI = await (await axios.get(`https://rickandmortyapi.com/api/character/${args.id}`)).data;
-    character.episode.map((episode) => {
-
-    })
     return (character);
+  },
+  charactersByIds: async (parent: any, args: { ids: number[] }) => {
+    let character: CharacterAPI[] = await (await axios.get(`https://rickandmortyapi.com/api/character/${args.ids.toString()}`)).data;
+    return character;
   }
 
 }
@@ -23,13 +24,22 @@ export const Character = {
   location: async (parent: { location: Location_json }) => {
     let last_location: Last_Location = (await (await axios.get(parent.location.url)).data);
     return last_location;
+  },
+  origin: async (parent: { origin: Location_json }) => {
+    if (parent.origin.name === "unknown") {
+      return parent.origin;
+    }
+    else {
+      let origin: Last_Location = (await (await axios.get(parent.origin.url)).data);
+      return origin;
+    }
   }
 
 }
 export const Episode = {
   characters: async (parent: { characters: string[] }) => {
     let characters = parent.characters.map(async (f) => {
-      let character = (await (await axios.get(f)).data) as EpisodeAPI;
+      let character = (await (await axios.get(f)).data) as CharacterAPI;
       return character;
     });
     return characters;
@@ -37,12 +47,11 @@ export const Episode = {
 }
 
 export const Location = {
-  residents: async (parent: { characters: string[] }) => {
-    console.log(parent.characters);
-    let characters = parent.characters.map(async (f) => {
-      let character = (await (await axios.get(f)).data) as EpisodeAPI;
-      return character;
-    });
-    return characters;
+  residents: async (parent: { residents: string[]}) => {
+  let residents = parent.residents.map(async (f) => {
+    let resident = (await (await axios.get(f)).data) as CharacterAPI;
+    return resident;
+  });
+  return residents;
   }
 }
